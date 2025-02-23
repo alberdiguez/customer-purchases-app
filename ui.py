5b6c65e041409010e44e50905230e576d1b98c61
alberdiguez <alberdiguez147@gmail.com>
2025-02-23 17:50:12 +0100

    Added more comments. Improved general UI: better filter visualization, headers, added bar chart for KPI clients_per_country.

--- ui.py
+++ ui.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.title("Customer Purchases App")
 
-# Tabs
+# Organize app in 2 different tabs
 tab1, tab2 = st.tabs(["Upload Purchases", "Analyse Purchases"])
 
 with tab1:
@@ -14,9 +14,12 @@ with tab1:
     country = st.text_input("Country", placeholder="Please enter a country")
     purchase_date = st.date_input("Purchase Date")
     amount = st.number_input("Amount", min_value=0.0)
+
     # Check if any field is empty
     disable_button = not (customer_name and country and purchase_date and amount)
+    # Button disabled if there are empty fields. Warns user to fill every field
     if st.button("Add Purchase", disabled=disable_button, help="All fields must be filled"):
+
         # Create dictionary to hold purchase details
         purchase_data = {
             'customer_name': customer_name,
@@ -24,8 +27,10 @@ with tab1:
             'purchase_date': purchase_date.isoformat(),
             'amount': amount        
         }
+
         # Send POST request to /purchase/
         response = requests.post("http://127.0.0.1:8000/purchase/", json=purchase_data)
+
         # Check API response
         if response.status_code == 200:
             st.success("Purchase added succesfully!")
@@ -34,7 +39,9 @@ with tab1:
 
     # Add multiple Purchases from CSV
     st.header("Add Purchases from CSV file")
+    # File uploader for CSV type files
     uploaded_file = st.file_uploader("Please select a CSV file", type='csv')
+
     if st.button("Upload"):
         if uploaded_file is not None:
             # Need to specify file name, content, and type
@@ -47,14 +54,20 @@ with tab1:
             else:
                 st.error("Error uploading purchases.")
         else:
-            st.error("Please select a file.")
+            st.error("Please select a file.")   # Display error if no file has been selected
 
 with tab2:
-    st.header("View Purchases")
     # Filters form
-    country_filter = st.text_input("Filter by Country")
-    start_date_filter = st.date_input("Start Date")
-    end_date_filter = st.date_input("End Date")
+    st.subheader("Filters")
+    # Organize filters in 3 columns
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        country_filter = st.text_input("Country")
+    with col2:
+        start_date_filter = st.date_input("Start Date")
+    with col3:
+        end_date_filter = st.date_input("End Date")
 
     if st.button("Get Purchases"):
         # Prepare query parameters
@@ -65,26 +78,38 @@ with tab2:
             params["start_date"] = start_date_filter.isoformat()
         if end_date_filter:
             params["end_date"] = end_date_filter.isoformat()
-
         # Make GET request to /purchases/
         response = requests.get("http://127.0.0.1:8000/purchases/", params=params)
         # Check API response
         if response.status_code == 200:
+            # Parse the JSON response
             result = response.json()
             purchases = result.get("purchases", [])
             kpis = result.get("kpis", None)
             # Display purchases if any match the given criteria
             if purchases:
+                st.header("Purchases")
                 df = pd.DataFrame(purchases)
-                st.dataframe(df)  # Show the DataFrame in Streamlit
+                # Rename columns
+                df.rename(columns={
+                    'customer_name': 'Customer Name',
+                    'country': 'Country',
+                    'purchase_date': 'Purchase Date',
+                    'amount': 'Amount'
+                }, inplace=True)
+                # Show the DataFrame in Streamlit
+                st.dataframe(df)
             else:
                 st.write("No purchases found for the given criteria")
-            # Display KPIs
+            # Display KPIs if avaliable
             if kpis:
-                st.subheader("KPIs")
-                st.write(f"**Mean Purchases per Client:** {kpis['avg_purchases_per_client']:.2f}")
-                st.write("**Clients per Country**")
-                for country, count in kpis['clients_per_country'].items():
-                    st.write(f"{country}: {count} client(s)")
+                st.header("KPIs")
+                st.subheader(f":blue-background[**Mean Purchases per Client:**] {kpis['avg_purchases_per_client']:.2f}")
+                st.subheader(":blue-background[**Clients per Country**]")
+                # Prepare data for clients per country
+                clients_per_country = kpis['clients_per_country']
+                # Create dataframe for clients_per_country KPI
+                clients_df = pd.DataFrame(list(clients_per_country.items()), columns=['Country', 'Number of Clients'])
+                st.bar_chart(clients_df.set_index('Country'))
         else:
             st.error("Error retrieving purchases.")
