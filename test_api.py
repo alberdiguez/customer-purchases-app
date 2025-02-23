c0eb0e41c9a60234b5fdc953aba4b2a5d0e1517a
alberdiguez <alberdiguez147@gmail.com>
2025-02-23 20:06:06 +0100

    Included GET purchases testing and invalid adding purchase data

--- test_api.py
+++ test_api.py
@@ -12,6 +12,7 @@ def test_api_running():
     # Verify succesful reponse
     assert response.status_code == 200
 
+# Test adding single purchase
 def test_add_purchase():
     # Prepare test data for single upload
     purchase_data = {
@@ -32,6 +33,7 @@ def test_add_purchase():
     assert response_data["country"] == purchase_data["country"]
     assert response_data["amount"] == purchase_data["amount"]
 
+# Test adding multiple purchases in bulk
 def test_bulk_upload_purchases():
     # Prepare test data for bulk upload
     bulk_data = """customer_name,country,purchase_date,amount
@@ -53,3 +55,32 @@ Victoria,France,2025-02-21,150.0
 
     # Clean up the temporary file
     os.remove(temp_csv_path)
+
+# Test GET request for purchases
+def test_get_purchases():
+    # Send GET request to /purchases/ endpoint to get purchases
+    response = httpx.get(f"{url_api}/purchases/")
+
+    # Verify if the response is successful
+    assert response.status_code == 200
+
+    # Check if the response contains purchases and if it is a list
+    response_data = response.json()
+    assert "purchases" in response_data
+    assert isinstance(response_data["purchases"], list)
+
+# Test invalid single purchase adding
+def test_add_purchase_invalid_data():
+    # Prepare invalid purchase data (missing required fields)
+    invalid_data = {
+        "customer_name": "",
+        "country": "Spain",
+        "purchase_date": "2023-02-20",
+        "amount": 500.0
+    }
+
+    # Send POST request to /purchase/ endpoint with invalid purchase
+    response = httpx.post(f"{url_api}/purchase/", json=invalid_data)
+
+    # Verify that the response is not successful (should return 422 Unprocessable Entity)
+    assert response.status_code == 422
